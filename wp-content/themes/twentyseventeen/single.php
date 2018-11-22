1d081966c5298bd47ce42fe168e4c817ba9cc69a
Vishal Pawar <vishal.pawar@tudip.nl>
2018-11-22 20:40:34 +0530

    Details: - Updated single post php file to show the two up module.

--- wp-content/themes/twentyseventeen/single.php
+++ wp-content/themes/twentyseventeen/single.php
@@ -10,34 +10,44 @@
  * @version 1.0
  */
 
-get_header(); ?>
+get_header();
+$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+?>
 
+<div class="post-container" style="background-image:url('/wp-content/uploads/2018/11/www.knobcreek.com-1564585437648699.jpg');background-repeat:no-repeat;background-size:cover;">
 <div class="wrap">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 			<?php
 				/* Start the Loop */
-				while ( have_posts() ) : the_post();
-
-					get_template_part( 'template-parts/post/content', get_post_format() );
-
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-
-					the_post_navigation( array(
-						'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'twentyseventeen' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '</span>%title</span>',
-						'next_text' => '<span class="screen-reader-text">' . __( 'Next Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'twentyseventeen' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ) . '</span></span>',
-					) );
-
-				endwhile; // End of the loop.
-			?>
+				while ( have_posts() ) : the_post();?>
+                    <div class="post-<?php echo get_the_ID();?> post-wrapper">
+                        <div class="post-wrapper__image">
+                            <img src="<?php echo $image[0]; ?>"/>
+                        </div>
+                        <div class="post-wrapper__description">
+                            <div class="post-wrapper__description__content">
+                                <div class="post__description--icon">
+                                    <i class="fas fa-file"></i>
+                                </div>
+                                <a href="<?php the_permalink(); ?>" class="post__description--date">
+                                    <button><?php echo get_the_date( 'F j' ); ?></button>
+                                </a>
+                                <a href="<?php the_permalink(); ?>" class="post__description--title">
+                                    <h4><?php the_title(); ?></h4>
+                                </a>
+                                <span><?php the_excerpt(); ?></span>
+                                <a href="<?php the_permalink(); ?>" class="post__description--readmore">									<h4> Read More </h4>
+                                </a>
+                            </div> <!-- .post-wrapper__description__content -->
+                        </div> <!-- .post-wrapper__description -->
+                    </div> <!-- .post-wrapper -->
+			<?php	endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-	<?php get_sidebar(); ?>
 </div><!-- .wrap -->
+</div><!-- .post-container -->
 
 <?php get_footer();
